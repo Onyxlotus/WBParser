0dcf510f2c6189f90d96f5bd8cf3970fb7bdba0f
Onyxlotus <onyxlotus@yandex.ru>
2023-01-20 21:28:47 +0300

    parser vk sharicat 2 format

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from bs4 import BeautifulSoup
 import undetected_chromedriver
-
+import pandas as pd
 import json
 import time
 
+
 def wildberries(url):
-
     options = undetected_chromedriver.ChromeOptions()
-
 
     driver = undetected_chromedriver.Chrome(
         executable_path="./config/chromedriver",
@@ -16,9 +15,9 @@ def wildberries(url):
 
     try:
         driver.get(url)
-        time.sleep(3)
+        time.sleep(1)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(5)
+        time.sleep(1)
         html = driver.page_source
         with open("main.html", "w", encoding='utf-8') as f:
             f.write(html)
@@ -35,35 +34,47 @@ def my_soup():
     with open("main.html", "r", encoding='utf-8') as f:
         file = f.read()
     soup = BeautifulSoup(file, "lxml")
-    all_product = soup.find_all(class_="product-card j-card-item j-good-for-listing-event") + \
-        soup.find_all(class_="product-card j-card-item")
+    all_product = soup.find_all(class_="market_row")
 
     for t, i in enumerate(all_product):
-        name = i.find('span', class_="goods-name").get_text()
-        brand = i.find('strong', class_="brand-name").get_text()
-        link = i.find("a", class_="j-card-link", href=True)['href']
-        try:
-            giver = " ".join(i.find("p", class_="product-card__delivery").find('b', class_="product-card__delivery-date").get_text().split())
-        except AttributeError:
-            giver = None
-        try:
-            price = " ".join(i.find('span', class_="price__wrap").find('ins', class_="price__lower-price").get_text().split())
-        except AttributeError:
-            price = None
+        name = i.find('div', class_="market_row_name").get_text()
+        brand = i.find('span', class_="market-item-price").get_text()
+        link = i.find("img", class_="market_row_img")['src']
 
         save_product[t] = {
-            "Бренд": brand,
+            "цена": brand,
             "Название": name,
-            "Цена": price,
-            "Ссылка": link,
-            "Доставка": giver
+
+            "Ссылка на фото": link,
+
         }
 
     with open('data.json', 'w', encoding='utf8') as outfile:
         json.dump(save_product, outfile, indent=4, ensure_ascii=False)
 
 
+def parse():
+    result_list = {'Название': [], 'Цена': [], 'Ссылка на фото': []}
+    with open("main.html", "r", encoding='utf-8') as f:
+        file = f.read()
+    soup = BeautifulSoup(file, "lxml")
+    xname = soup.find_all(class_="market_row_name")
+    brand = soup.find_all(class_="market-item-price")
+    link = soup.find("img", class_="market_row_img")['src']
+
+    for name in xname:
+        result_list['Название'].append(name.text)
+    for name in brand:
+        result_list['Цена'].append(name.text)
+    for name in link:
+        result_list['Ссылка на фото'].append(name.text)
+    return result_list
+
+
 if __name__ == '__main__':
-    my_url = "https://www.wildberries.ru/catalog/elektronika/igry-i-razvlecheniya/aksessuary/garnitury"
+    my_url = "https://vk.com/market-202261956"
     wildberries(my_url)
+    df = pd.DataFrame(data=parse())
+    df.to_excel("data.xlsx")
     my_soup()
+
